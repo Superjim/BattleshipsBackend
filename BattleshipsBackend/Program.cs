900e0cc080fc71ebbf973bdcebd5fef6f5cdac7e
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-08-01 22:33:26 +0100

    round 2 worked

--- BattleshipsBackend/Program.cs
+++ BattleshipsBackend/Program.cs
@@ -10,10 +10,20 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "MyAPI");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
