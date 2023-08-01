5fe4e445a945d655c51c27b4e7959e6223fcd2d6
Jim Jenkinson <jamesjenkinson10@gmail.com>
2023-08-01 21:42:02 +0100

    Added startup.cs

---
+++ BattleshipsBackend/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace BattleshipsBackend
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Battleships API",
+                    Version = "v1",
+                    Description = "API for Battleships game",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Your Name",
+                        Email = "your.email@example.com"
+                    }
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Battleships API v1");
+            });
+        }
+    }
+}
